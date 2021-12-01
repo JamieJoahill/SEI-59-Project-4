bb552fba3252ace644b957334e0b8d7da5f68c90
Jamie Joahill <jamie.joahill@gmail.com>
2021-12-01 21:21:47 +0000

    EventListView & EventDetailView

--- events/views.py
+++ events/views.py
@@ -1,3 +1,4 @@
+from os import stat
 from django.shortcuts import render
 from django.http.response import HttpResponse
 from rest_framework.views import APIView
@@ -8,13 +9,33 @@ from .serializers import EventSerializer
 # Create your views here.
 
 class EventDetailView(APIView):
+
+    # Delete an event
+    def delete(self, _request, pk):
+        try:
+            event = Event.object.get(id=pk)
+        except:
+            return Response(status=status.HTTP_500_INTERNALSERVER_ERROR)
+        event.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    # Update an event
+    def put(self, request, pk):
+        event = Event.objects.get(id=pk)
+        updated_event = EventSerializer(event, data=request.data)
+        if updated_event.is_valid():
+            updated_event.save()
+            return Response(updated_event.data, status=status.HTTP_202_ACCEPTED)
+        else:
+            return Respinse(updated_event.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
     def get(self, _request, pk):
         event = Event.objects.get(id=pk)
         serialized_event = EventSerializer(event)
         return Response(serialized_event.data, status=status.HTTP_200_OK)
 
 class EventListView(APIView):
-
+    # Create/POST an event
     def post(self, request):
         event = EventSerializer(data = request.data)
         if event.is_valid():
@@ -22,7 +43,7 @@ class EventListView(APIView):
             return Response(event.data, status=status.HTTP_201_CREATED)
         else:
             return Response(event.errros, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
-
+    # GET an event
     def get(self, _request):
         events = Event.objects.all()
         serialized_events = EventSerializer(events, many=True)
