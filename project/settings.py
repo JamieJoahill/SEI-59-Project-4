ba559821027ef7300a58bdb6220ec5eb80323214
Jamie Joahill <jamie.joahill@gmail.com>
2022-01-03 01:20:16 +0000

    settings.py

--- project/settings.py
+++ project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import django_on_heroku
 import os
 from pathlib import Path
 
@@ -197,3 +197,4 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'client', "build", "static"),
 )
 
+django_on_heroku.settings(locals())
