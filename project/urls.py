1b45690b4759bdcda1a853684e9d857368b62df2
Jamie Joahill <jamie.joahill@gmail.com>
2022-01-03 00:11:24 +0000

    predeployment

--- project/urls.py
+++ project/urls.py
@@ -14,8 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
-from django.urls.conf import include
+from django.urls import path, include, re_path
 from .views import index
 
 urlpatterns = [
